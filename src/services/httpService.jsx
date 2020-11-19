92bfe836f4e5badc809d765485b615406245f237
Jon Fleet <jmfleet1@gmail.com>
2020-11-19 13:42:29 -0500

    Production Config

--- src/services/httpService.jsx
+++ src/services/httpService.jsx
@@ -2,11 +2,12 @@ import axios from "axios";
 
 axios.defaults.baseURL = process.env.REACT_APP_API_URL
 axios.defaults.headers.common['x-auth-token'] = localStorage.getItem('jwt');
-// axios.defaults.headers.common['Access-Control-Allow-Origin'] = "http://localhost:3700"
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = process.env.REACT_APP_API_URL
 
+// axios.defaults.headers.common['Access-Control-Allow-Origin'] = process.env.REACT_APP_API_URL
+
+console.log(process.env.REACT_APP_API_URL)
 axios.interceptors.request.use( (request) => {
-    // console.log(request)
+    console.log(request)
     return request
     }, (error) => {
     
