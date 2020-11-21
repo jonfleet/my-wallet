e7f5794921804ace58717514240cecafa518a295
Jon Fleet <jmfleet1@gmail.com>
2020-11-21 14:51:46 -0500

    remove .env production and dev files

--- src/services/httpService.jsx
+++ src/services/httpService.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL
+// axios.defaults.baseURL = process.env.REACT_APP_API_URL
 axios.defaults.headers.common['x-auth-token'] = localStorage.getItem('jwt');
 
 // test
 // axios.defaults.headers.common['Access-Control-Allow-Origin'] = process.env.REACT_APP_API_URL
 
-console.log(process.env.REACT_APP_API_URL)
+console.log("Private Message: ", process.env.PRIVATE_MESSAGE)
 axios.interceptors.request.use( (request) => {
     console.log(request)
     return request
