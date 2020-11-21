3c000a0ef399787f0619c62643adfb122abd184c
Jon Fleet <jmfleet1@gmail.com>
2020-11-20 20:43:41 -0500

    CORS Troubleshooting

--- src/services/httpService.jsx
+++ src/services/httpService.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 axios.defaults.baseURL = process.env.REACT_APP_API_URL
 axios.defaults.headers.common['x-auth-token'] = localStorage.getItem('jwt');
 
+// test
 // axios.defaults.headers.common['Access-Control-Allow-Origin'] = process.env.REACT_APP_API_URL
 
 console.log(process.env.REACT_APP_API_URL)
